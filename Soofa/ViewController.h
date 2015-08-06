fc7cb251f931619f277d32f6ed3ce30c0ba56736
Bob Breznak <bob.breznak@gmail.com>
2015-08-06 10:08:39 -0400

    Initial Commit

---
+++ Soofa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Soofa
+//
+//  Created by Robert Breznak on 8/6/15.
+//  Copyright (c) 2015 Soofa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Soofa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Soofa
+//
+//  Created by Robert Breznak on 8/6/15.
+//  Copyright (c) 2015 Soofa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
