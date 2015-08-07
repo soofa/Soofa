8ce8df83295b8b2a18e0fd8aacce686d43c8c1b9
Bob Breznak <bob.breznak@gmail.com>
2015-08-07 10:00:18 -0400

    Setup MapView and display bench location with current location

--- Soofa/ViewController.m
+++ Soofa/ViewController.m
@@ -8,6 +8,10 @@
 
 #import "ViewController.h"
 
+#define MINIMUM_ZOOM_ARC 0.014 //approximately 1 miles (1 degree of arc ~= 69 miles)
+#define ANNOTATION_REGION_PAD_FACTOR 1.15
+#define MAX_DEGREES_ARC 360
+
 @interface ViewController ()
 
 @end
@@ -16,12 +20,113 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.mapView.delegate = self;
+    self.mapView.mapType = MKMapTypeStandard;
+    self.mapView.showsUserLocation = YES;
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    [[self locationManager] setDelegate:self];
+
+    // we have to setup the location maanager with permission in later iOS versions
+    if ([[self locationManager] respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [[self locationManager] requestWhenInUseAuthorization];
+    }
+    
+    MKPointAnnotation *benchAnnotation;
+    benchAnnotation = [[MKPointAnnotation alloc]init];
+    benchAnnotation.coordinate = CLLocationCoordinate2DMake(42.368523, -71.100102);
+    benchAnnotation.title = @"Maria";
+    benchAnnotation.subtitle = @"Lives in Sennott Park Cambridge, MA";
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://s3.amazonaws.com/api.soofa.io/devices.geojson"]];
+    
+    __block NSDictionary *json;
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               json = [NSJSONSerialization JSONObjectWithData:data
+                                                                      options:0
+                                                                        error:nil];
+                               NSLog(@"Async JSON: %@", json);
+                           }];
+    
+    [self.mapView addAnnotation:benchAnnotation];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self zoomMapViewToFitAnnotations:self.mapView animated:animated];
+    //or maybe you would do the call above in the code path that sets the annotations array
+}
+
+#pragma mark Delegate Methods
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    id <MKAnnotation> annotation = [view annotation];
+    
+    if ([annotation isKindOfClass:[MKPointAnnotation class]]) {
+        NSLog(@"Clicked Pizza Shop");
+    }
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Disclosure Pressed" message:@"Click Cancel to Go Back" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
+    
+    [alertView show];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    // If it's the user location, just return nil.
+    if ([annotation isKindOfClass:[MKUserLocation class]]) {
+        return nil;
+    }
+    
+    // Handle any custom annotations.
+    if ([annotation isKindOfClass:[MKPointAnnotation class]])
+    {
+        // Try to dequeue an existing pin view first.
+        MKAnnotationView *pinView = (MKAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
+        if (!pinView)
+        {
+            // If an existing pin view was not available, create one.
+            pinView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"CustomPinAnnotationView"];
+            pinView.canShowCallout = YES;
+            pinView.image = [UIImage imageNamed:@"bench-base-standalone-icon"];
+            pinView.calloutOffset = CGPointMake(0, 0);
+            
+            // Add a detail disclosure button to the callout.
+            UIButton* rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+            pinView.rightCalloutAccessoryView = rightButton;
+            
+            // Add an image to the left callout.
+            UIImageView *iconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bench-base-standalone-icon"]];
+            pinView.leftCalloutAccessoryView = iconView;
+        } else {
+            pinView.annotation = annotation;
+        }
+        return pinView;
+    }
+    return nil;
+}
+
+#pragma mark Zoom
+
+- (void)zoomMapViewToFitAnnotations:(MKMapView *)mapView animated:(BOOL)animated
+{
+    CLLocation *location = [self.locationManager location];
+    
+    NSMutableArray *annotations = [[NSMutableArray alloc] initWithObjects:location, nil];
+    [annotations addObjectsFromArray:mapView.annotations];
+    
+    int count = [annotations count];
+    if ( count == 0) { return; } //bail if no annotations
+    
+    [self.mapView showAnnotations:self.mapView.annotations animated:YES];
 }
 
 @end
